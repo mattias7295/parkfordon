53f8278de83fd834f0a619a2ac0a005174c536a5
Mattias <mattiasscherer@gmail.com>
2014-05-08 16:16:04 +0200

    Ett projekt för kontrollen
    Skiv kod här för att programmera kontrollen

---
+++ kontroll/kontroll/kontroll.c
@@ -0,0 +1,17 @@
+/*
+ * kontroll.c
+ *
+ * Created: 2014-05-08 16:14:54
+ *  Author: Rymdleffe
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
