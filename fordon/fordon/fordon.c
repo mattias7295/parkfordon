f20a75899dda7204ae981b6602c731c26c7c183c
Mattias <mattiasscherer@gmail.com>
2014-05-08 16:14:01 +0200

    Skapade projekt för fordon
    Använd detta projekt när ni ska programmera fordon

---
+++ fordon/fordon/fordon.c
@@ -0,0 +1,17 @@
+/*
+ * fordon.c
+ *
+ * Created: 2014-05-08 16:13:26
+ *  Author: Rymdleffe
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
