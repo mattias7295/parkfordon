9e7971259c1c1f576d34e1e1e930e22c261c74c0
Mattias <mattiasscherer@gmail.com>
2014-05-08 20:18:48 +0200

    lagt till usart och adc till fordon

---
+++ fordon/fordon/adc.h
@@ -0,0 +1,18 @@
+/*
+ * adc.h
+ *
+ * Created: 2014-05-08 20:11:36
+ *  Author: Mattias
+ */ 
+
+#include <avr/io.h>
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#define ADC_PIN			5
+#define ADC_THRESHOLD	512
+
+uint8_t adc_read(uint8_t adcx);
+
+#endif /* ADC_H_ */
--- fordon/fordon/fordon.c
+++ fordon/fordon/fordon.c
@@ -2,14 +2,18 @@
  * fordon.c
  *
  * Created: 2014-05-08 16:13:26
- *  Author: Rymdleffe
+ *  Author: Mattias
  */ 
 
-
 #include <avr/io.h>
+#include "usart.h"
+
 
 int main(void)
 {
+	// Init USART
+	USART_Init(MYUBRR);
+	
     while(1)
     {
         //TODO:: Please write your application code 
