05a4f4396fbfc093cfc237232e15e65f30ed1f18
johe0179 <zzzanoy@gmail.com>
2014-05-09 13:46:05 +0200

    TWI

--- fordon/fordon/Debug/twi.d
+++ fordon/fordon/Debug/twi.d
@@ -10,7 +10,11 @@ twi.d twi.o: .././twi.c \
  c:\program\ files\ (x86)\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/version.h \
  c:\program\ files\ (x86)\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/fuse.h \
  c:\program\ files\ (x86)\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/lock.h \
- .././twi.h
+ .././twi.h \
+ c:\program\ files\ (x86)\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay.h \
+ c:\program\ files\ (x86)\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay_basic.h \
+ c:\program\ files\ (x86)\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/math.h \
+ .././usart.h
 
 c:\program\ files\ (x86)\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/io.h:
 
@@ -35,3 +39,11 @@ c:\program\ files\ (x86)\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.
 c:\program\ files\ (x86)\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/lock.h:
 
 .././twi.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/math.h:
+
+.././usart.h:
--- fordon/fordon/twi.c
+++ fordon/fordon/twi.c
@@ -6,23 +6,51 @@
  */ 
 #include<avr/io.h> 
 #include "twi.h"
-void twi_init()
-{
-	TWBR=72;
-}
+#include <util/delay.h>
+#include "usart.h"
 
 void compas_update()
 {
-	TWCR=(1<<TWINT)|(1<<TWEN)|(1<<TWSTA);
-	while(!((TWCR)&(1<<TWINT)));
+	//Start
+	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
+	while (!(TWCR & (1<<TWINT)));
 	
-	TWDR=0x42; 
 	
-	TWCR=(1<<TWINT)|(1<<TWEN);
-	while(!((TWCR)&(1<<TWINT)));
+	//Send address
+	TWDR = 0x42;
+	TWCR = (1<<TWINT) | (1<<TWEN);
+	while (!(TWCR & (1<<TWINT)))
 	
-	TWDR=0x41;
-	TWCR=(1<<TWINT)|(1<<TWEN);
-	while(!((TWCR)&(1<<TWINT)));
-	USART_Transmit(TWDR);
+	
+	//Send Data
+	TWDR = 0x41;
+	TWCR = (1<<TWINT) | (1<<TWEN);
+	while (!(TWCR & (1<<TWINT)))
+	
+	//Stop
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);	
+	_delay_ms(70);
+	
+	//Start
+	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
+	while (!(TWCR & (1<<TWINT)));
+	
+	
+	//Send address
+	TWDR = 0x43;
+	TWCR = (1<<TWINT) | (1<<TWEN);
+	while (!(TWCR & (1<<TWINT)))
+	
+	//Read data
+	TWCR = (1<<TWINT) | (1<<TWEA);
+	uint8_t  high = TWDR;
+	USART_Transmit(high);
+	
+	TWCR = (1<<TWINT);
+	uint8_t low = TWDR;
+	USART_Transmit(low);
+	
+	
+	//Stop
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
 }
