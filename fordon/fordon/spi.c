d31386fabf00b76c07e5429857372e99dea18104
mattias <mattiasscherer@gmail.com>
2014-05-09 12:29:11 +0200

    dxcxcx

--- fordon/fordon/spi.c
+++ fordon/fordon/spi.c
@@ -12,6 +12,7 @@ void SPI_MasterInit(void)
 {
 	/* Set MOSI and SCK output, all others input */
 	DDRB = (1<<PB5)|(1<<PB7);
+	PORTB |= (1<<PB5)|(1<<PB7);
 	/* Enable SPI, Master, set clock rate fck/16 */
 	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
 }
@@ -20,10 +21,10 @@ void SPI_MasterTransmit(char cData)
 	/* Start transmission */
 	SPDR = cData;
 	/* Wait for transmission complete */
-	while(!(SPSR & (1<<SPIF)))
+	while(!(SPSR & (1<<SPIF)))	
 	;
 	cData = SPDR;
 	
-	USART_Transmit(cData >> 8);
-	USART_Transmit(cData);
+	//USART_Transmit(cData >> 7);
+	//USART_Transmit(6);
 }
