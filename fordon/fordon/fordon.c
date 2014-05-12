06895e55c5a577ab4071dfad7dd2abd98a37770e
johe0179 <zzzanoy@gmail.com>
2014-05-12 15:10:15 +0200

    PWM

--- fordon/fordon/fordon.c
+++ fordon/fordon/fordon.c
@@ -13,11 +13,15 @@
 #include "pwm.h"
 #include "spi.h"
 #include "twi.h"
+
+#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
+
 int main(void)
 {
 	USART_Init(MYUBRR);
+	init_pwm();
 	adc_init();
-	char cdata;
+
 	PORTC |= (1<<PC0)|(1<<PC1);
     while(1)
     {
@@ -25,4 +29,54 @@ int main(void)
 		_delay_ms(100);
         
     }
+}
+
+void parseBluetooth(unsigned char command) {
+
+	uint8_t speed = 0;
+	if (CHECK_BIT(command, 7))
+	{
+		speed = (command >> 4) & 0x7;
+		if (speed == 0)
+		{
+			speed = 0xff;
+		}
+		else {
+			speed = 255-(speed * 36);		
+		}
+		initEngineLeftForward((unsigned char)speed);
+	}
+	else {
+		speed = (command >> 4) & 0x7;
+		if (speed == 0)
+		{
+			speed = 0xff;
+		}
+		else {
+			speed = 255-(speed * 36);
+		}
+		initEngineLeftBackward((unsigned char)speed);
+	}
+	if (CHECK_BIT(command, 3)) {
+		speed = command & 0x7;
+		if (speed == 0)
+		{
+			speed = 0xff;
+		}
+		else {
+			speed = 255-(speed * 36);
+		}
+		initEngineRightForward((unsigned char)speed);
+	}
+	else {
+		speed = command & 0x7;
+		if (speed == 0)
+		{
+			speed = 0xff;
+		}
+		else {
+			speed = 255-(speed * 36);
+		}
+		initEngineRightBackward((unsigned char)speed);
+	}
 }
