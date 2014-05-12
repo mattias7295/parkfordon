629b40ed5f3005cbae312caa5c9a2c4dc68ad80c
johe0179 <zzzanoy@gmail.com>
2014-05-12 09:51:02 +0200

    TWI

--- fordon/fordon/twi.c
+++ fordon/fordon/twi.c
@@ -17,52 +17,54 @@ void compas_update()
 	
 	
 	//Send address
-	TWDR = 0x43;
+	TWDR = 0x42;
 	TWCR = (1<<TWINT) | (1<<TWEN);
 	while (!(TWCR & (1<<TWINT)));
-	
-	//Read data	
+		
+		
+	//Send Data
+	TWDR = 0x41;
 	TWCR = (1<<TWINT) | (1<<TWEN);
-	while(!(TWCR & (1<<TWINT)));
-	uint16_t high = TWDR;
-	USART_Transmit(high);
-	
+	while (!(TWCR & (1<<TWINT)));
+		
 	//Stop
 	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
-	
+	_delay_ms(70);
+		
 	//Start
 	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
 	while (!(TWCR & (1<<TWINT)));
-	
+		
+		
 	//Send address
 	TWDR = 0x43;
 	TWCR = (1<<TWINT) | (1<<TWEA);
 	while (!(TWCR & (1<<TWINT)));
-	
-	//Read data	
-	TWCR = (1<<TWINT);
+		
+	//Read data
+	TWCR = (1<<TWINT) | (1<<TWEA);
 	while(!(TWCR & (1<<TWINT)));
-	high = TWDR;
-
+	uint16_t high = TWDR;
 	USART_Transmit(high);
-	
+		
 	//Start
 	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
 	while (!(TWCR & (1<<TWINT)));
-	
-	
+		
+		
 	//Send address
 	TWDR = 0x43;
 	TWCR = (1<<TWINT) | (1<<TWEN);
 	while (!(TWCR & (1<<TWINT)));
-	
+		
 	//Read data
 	TWCR = (1<<TWINT) | (1<<TWEN);
 	while(!(TWCR & (1<<TWINT)));
 	high = TWDR;
-
+	high = TWDR;
 	USART_Transmit(high);
-	
+		
+
 	//Stop
 	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
 }
