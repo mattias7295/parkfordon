03e82c90e2f53964b6af8f27f0d407087a2c5e90
mattias <mattiasscherer@gmail.com>
2014-05-19 10:18:27 +0200

    printf

--- fordon/fordon/adc.c
+++ fordon/fordon/adc.c
@@ -7,10 +7,14 @@
 
 #include "adc.h"
 
+static void put_char(uint8_t c, FILE* stream);
+static FILE mystdout = FDEV_SETUP_STREAM(put_char, NULL, _FDEV_SETUP_WRITE);
+
 void adc_init() {
-	
+	stdout = &mystdout;
 	/* Enable the ADC */
 	ADCSRA |= _BV(ADEN);
+	printf("testing %d\n\r", i);
 }
 
 uint8_t adc_read(uint8_t adcx) {
@@ -26,4 +30,11 @@ uint8_t adc_read(uint8_t adcx) {
 	
 	ADMUX |= (1 << ADLAR);
 	return ADCH;
+}
+
+static void put_char(uint8_t c, FILE* stream)
+{
+	if (c == '\n') put_char('\r', stream);
+	while(!(UCSR0A & (1 << UDRE0)));
+	UDR0 = c;
 }
