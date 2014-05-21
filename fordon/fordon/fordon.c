7866c25ae16c95caf23c997332a3d6e47d5867fd
johe0179 <zzzanoy@gmail.com>
2014-05-21 09:52:02 +0200

    sadaasd

--- fordon/fordon/fordon.c
+++ fordon/fordon/fordon.c
@@ -211,7 +211,7 @@ int main(void)
 		
 	}
 	int x = 0;
-	while(x<5)
+	/*while(x<5)
 	{
 		if(!(PINB & _BV(PB2)))
 		{
@@ -222,27 +222,27 @@ int main(void)
 			x = 0;
 		}
 		_delay_ms(4000);
-	}
+	}*/
 	_delay_ms(8000);
     while(1)
     {
-		//USART_Transmit(0xff);
-		//parseBluetooth(USART_Receive());
+		USART_Transmit(0xff);
+		parseBluetooth(USART_Receive());
 		//printf("Kompass:%d\n",compas_update());
 		// Wait for fix
-		calcHeading();
+		//calcHeading();
 
 		_delay_ms(8000);
     }
 
 }
 
-int calcHeading() {
+void calcHeading() {
 	double latPerson;
 	double lonPerson;
 	char latitude[10];
 	char longitude[11];
-	/*USART_Transmit(0xFF);
+	USART_Transmit(0xFF);
 	for(int i = 0;i<10;i++) {
 		latitude[i] = USART_Receive();
 		if(latitude[i] == 0x00) {
@@ -258,53 +258,34 @@ int calcHeading() {
 	}
 	
 	char degA[3];
-	degA[0] = latitude[0];
-	degA[1] = latitude[1];
+	strncpy(degA, latitude,2);
 	degA[2] = '\0';
-	
-	double deg = atof(degA);
-	
+
 	char minA[8];
-	minA[0] = latitude[2];
-	minA[1] = latitude[3];
-	minA[2] = latitude[4];
-	minA[3] = latitude[5];
-	minA[4] = latitude[6];
-	minA[5] = latitude[7];
-	minA[6] = latitude[8];
+	strncpy(minA,latitude+2,7);
 	minA[7] = '\0';
-	
-	double minutes = atof(minA);
-	latPerson = deg + minutes/60;
-	
-	char degO[4];
-	degO[0] = longitude[0];
-	degO[1] = longitude[1];
-	degO[2] = longitude[2];
-	degO[3] = '\0';
-	
-	deg  = atof(degO);
-	
-	char minO[8];
-	minO[0] = longitude[3];
-	minO[1] = longitude[4];
-	minO[2] = longitude[5];
-	minO[3] = longitude[6];
-	minO[4] = longitude[7];
-	minO[5] = longitude[8];
-	minO[6] = longitude[9];
-	minO[7] = '\0';
-	
-	minutes = atof(minO);
-	lonPerson = deg + minutes/60;*/
+
 
 	
-	parseGPS();
+	double d,e;
+	d = strtod(degA,NULL);
+	e = strtod(minA,NULL);
+	latPerson = (d + e/60);
+	
+	strncpy(degA,longitude+1,2);
+	degA[2] = '\0';
+	strncpy(minA, longitude+3,7);
+	minA[7] = '\0';
+	d = strtod(degA,NULL);
+	e = strtod(minA,NULL);
+	lonPerson = (d + e/60);
+	printf("Lat: %lf \nLon: %lf \n", latPerson, lonPerson);
+	/*parseGPS();
 	latPerson = 63.820374;
 	lonPerson = 20.308906;
 
 
-	printf("Lat: %lf \nLon: %lf \n", lat, lon);
+	
 	// Räkna ut vilken riktning fordonet ska vända sig åt av argumenten
 	
 	// Om vinkeln stämmer ungefär, kör framåt tills koordinaterna överenstämmer
@@ -316,8 +297,7 @@ int calcHeading() {
 		prevSpeedR = 100;
 		_delay_ms(500);
 		
-	}
-	return 0;		
+	}*/
 	
 }
 void spin(double latPerson, double lonPerson) 
